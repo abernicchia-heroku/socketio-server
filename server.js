83f92884f6a3673db4cdf43324fff05963d58821
Andrea Bernicchia <abernicchia@abernic-ltmrz50.internal.salesforce.com>
2023-02-11 21:41:31 +0100

    socket.io starting point

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ let
 
 // event fired every time a new client connects:
 server.on("connection", (socket) => {
-    console.info(`Client connected [id=${socket.id}]`);
     // initialize this client's sequence number
     sequenceNumberByClient.set(socket, 1);
+    console.info(`Client connected [id=${socket.id}] clients: ${sequenceNumberByClient.size}`);
 
     // when socket disconnects, remove it from the list:
     socket.on("disconnect", () => {
         sequenceNumberByClient.delete(socket);
-        console.info(`Client gone [id=${socket.id}]`);
+        console.info(`Client gone [id=${socket.id}] clients: ${sequenceNumberByClient.size}`);
     });
 });
 
