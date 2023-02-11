6e63334c1a994afc877a28f8b3b70bffc7647176
Andrea Bernicchia <abernicchia@abernic-ltmrz50.internal.salesforce.com>
2023-02-11 16:35:10 +0100

    socket.io starting point

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
+// const express = require('express');
+// const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+// const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
+const PORT = process.env.PORT || 8000;
+
+
+const
+    {Server} = require("socket.io"),
+    server = new Server(${PORT});
+
+let
+    sequenceNumberByClient = new Map();
+
+// event fired every time a new client connects:
+server.on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    // initialize this client's sequence number
+    sequenceNumberByClient.set(socket, 1);
+
+    // when socket disconnects, remove it from the list:
+    socket.on("disconnect", () => {
+        sequenceNumberByClient.delete(socket);
+        console.info(`Client gone [id=${socket.id}]`);
+    });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// sends each client its current sequence number
+setInterval(() => {
+    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+        client.emit("seq-num", sequenceNumber);
+        sequenceNumberByClient.set(client, sequenceNumber + 1);
+    }
+}, 1000);
